7c05823e93bd4d7c2e8ae86f0033542b7ec8d3e1
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-06 23:24:29 -0300

    add composables

--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,35 +1,29 @@
 <script setup> 
-    import axios from 'axios'
-    import { ref } from 'vue';
     import { RouterLink } from 'vue-router';
+    import { useGetData } from '@/composables/getData';
 
-    let pokemons = ref([])
+    const { data, getData, loading } = useGetData()
 
-    const getData = async() => {
-        try {
-            const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-            pokemons.value = data.results
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    getData()
+    getData('https://pokeapi.co/api/v2/pokemon/')
 </script>
 
 <template>
     <h1>Pokemons</h1>
-    <ul class="list-group">
-        <li
-            v-for="pokemon in pokemons"
-            class="list-group-item"
-        > 
-            <router-link    
-                :to="`/pokemons/${pokemon.name}`" 
-                class="nav-link">
-                {{ pokemon.name[0].toUpperCase() + pokemon.name.slice(1) }}
-            </router-link>
-            
-        </li>    
-    </ul>
+    <p v-if="loading">Loading...</p>
+    <div v-if="data">
+        <ul class="list-group">
+            <li
+                v-for="pokemon in data.results"
+                class="list-group-item"
+            > 
+                <router-link    
+                    :to="`/pokemons/${pokemon.name}`" 
+                    class="nav-link">
+                    {{ pokemon.name[0].toUpperCase() + pokemon.name.slice(1) }}
+                </router-link>
+                
+            </li>    
+        </ul>
+    </div>
+
 </template>
